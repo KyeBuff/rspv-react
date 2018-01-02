c5c3204c5e7175e1f44761b68c260743ac39c9ae
KyeBuff <kye.buffery@btinternet.com>
2018-01-02 11:38:39 +0000

    tidied up form props

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 let Form = (props) => (
   <form onSubmit={props.onFormSubmit}>
-    <input type="text" onChange={props.newPendingGuest} value={props.value} placeholder="Invite Someone" />
-    <button type="submit" name="submit" value="submit">Submit</button>
+    <input 
+    	type="text" 
+    	onChange={props.newPendingGuest} 
+    	value={props.value} 
+    	placeholder="Invite Someone" 
+    />
+    <button 
+    	type="submit" 
+    	name="submit" 
+    	value="submit"
+    >
+    Submit
+    </button>
   </form>
 );    
 
